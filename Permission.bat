25d878ff46d3ca0f468e4b9dbb01e29c87687a0c
den-gol-devops <117167396+den-gol-devops@users.noreply.github.com>
2024-03-03 06:51:42 +0200

    Update Permission.bat

--- Permission.bat
+++ Permission.bat
@@ -3,7 +3,7 @@ cd E:\denis\DevOps
 
 icacls ./dev-app.pem /inheritance:r
 
-icacls ./dev-app.pem /grant denis_golkov:F
+icacls ./dev-app.pem /grant "user":F
 icacls .\dev-app.pem /remove "Authenticated Users"
 icacls ./dev-app.pem /remove SYSTEM
 icacls ./dev-app.pem /remove Administrators
